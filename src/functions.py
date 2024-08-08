f057df41ae7897d88277be50bf13e82d2a65ab3d
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-08-08 20:19:14 +0100

    ensure whitespace is ignored

--- src/functions.py
+++ src/functions.py
@@ -1,5 +1,5 @@
 from typing import List
 
 def parse_db_inspect_input(input: str) -> List[str]:
-    input_keys = [x for x in input.split(',') if len(x) > 0]
+    input_keys = [x for x in input.replace(' ', '').split(',') if len(x) > 0]
     return input_keys
--- test.py
+++ test.py
@@ -6,3 +6,13 @@ def test_parse_input_returns_empty_list_when_given_empty_input():
     result = parse_db_inspect_input('')
     assert(isinstance(result, list))
     assert(0 == len(result))
+    
+def test_parse_input_returns_empty_list_when_given_whitespace():
+    result = parse_db_inspect_input('  ')
+    assert(0 == len(result))
+
+# def test_parse_input_returns_empty_list_when_given_just_commas():
+#     pass
+
+# def test_parse_input_returns_empty_list_when_given_commans_and_whitespace():
+#     pass
