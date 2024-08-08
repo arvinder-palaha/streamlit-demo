c9999ef87a3a1662ff0103370cb8d5a7f1414942
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-08-08 20:24:14 +0100

    parametrised tests for empty list returned

--- test.py
+++ test.py
@@ -2,19 +2,13 @@ import pytest
 
 from src.functions import parse_db_inspect_input
 
-def test_parse_input_returns_empty_list_when_given_empty_input():
-    result = parse_db_inspect_input('')
-    assert(isinstance(result, list))
-    assert(0 == len(result))
-    
-def test_parse_input_returns_empty_list_when_given_whitespace():
-    result = parse_db_inspect_input('  ')
-    assert(0 == len(result))
-
-def test_parse_input_returns_empty_list_when_given_just_commas():
-    result = parse_db_inspect_input(',,,')
-    assert(0 == len(result))
-
-def test_parse_input_returns_empty_list_when_given_commans_and_whitespace():
-    result = parse_db_inspect_input(', ,  ,   ')#
-    assert(0 == len(result))
+@pytest.mark.parametrize("input_string", [
+    '',           # Empty input
+    '  ',         # Whitespace
+    ',,,',        # Just commas
+    ', ,  ,   '   # Commas and whitespace
+])
+def test_parse_input_returns_empty_list(input_string):
+    result = parse_db_inspect_input(input_string)
+    assert isinstance(result, list)
+    assert len(result) == 0
