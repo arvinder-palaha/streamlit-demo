5e1d4cd355b61e7ff1a935d3fcce024b5fe315a0
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-08-08 20:05:36 +0100

    corrected test name

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import pytest
 
 from src.functions import parse_db_inspect_input
 
-def test_parse_input_returns_empty_list_when_given_whitespace():
+def test_parse_input_returns_empty_list_when_given_empty_input():
     result = parse_db_inspect_input('')
     assert(isinstance(result, list))
     assert(0 == len(result))
