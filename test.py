3fe968ef7749a59c1e0af40d040fa270fbeb0f1e
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-08-08 20:34:12 +0100

    test for returning just db_name (1st input key)

--- test.py
+++ test.py
@@ -12,3 +12,14 @@ def test_parse_input_returns_empty_list(input_string):
     result = parse_db_inspect_input(input_string)
     assert isinstance(result, list)
     assert len(result) == 0
+
+@pytest.mark.parametrize("input_string", [
+    'db_name',
+    ',db_name,',
+    '  db_name  ',
+    ' , db_name, '
+])
+def test_parse_input_returns_only_dbname(input_string):
+    result = parse_db_inspect_input(input_string)
+    assert len(result) == 1
+    assert result[0] == 'db_name'
