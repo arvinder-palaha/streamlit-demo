0698c0c40ead737bba06344d168070d0a4a31b00
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-08-08 20:57:52 +0100

    keep spaces in key names

--- src/functions.py
+++ src/functions.py
@@ -1,5 +1,7 @@
 from typing import List
 
 def parse_db_inspect_input(input: str) -> List[str]:
-    input_keys = [x for x in input.replace(' ', '').split(',') if len(x) > 0]
-    return input_keys
+    split_input = input.split(',')
+    stripped_split_input = [x.lstrip().rstrip() for x in split_input]
+    filtered_stripped_split_input = [x for x in stripped_split_input if len(x)>0]
+    return filtered_stripped_split_input
