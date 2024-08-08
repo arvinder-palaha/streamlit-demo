9b6c0c382acd979edbbb73e0010798e3ff67a344
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-08-08 20:44:05 +0100

    test parse 2 input keys

--- test.py
+++ test.py
@@ -23,3 +23,12 @@ def test_parse_input_returns_only_dbname(input_string):
     result = parse_db_inspect_input(input_string)
     assert len(result) == 1
     assert result[0] == 'db_name'
+    
+@pytest.mark.parametrize("input_string", [
+    'db_name,col_name',
+    'db_name, col_name  , ',
+    ', ,db_name,col_name'
+])
+def test_parse_input_returns_2_keys(input_string):
+    result = parse_db_inspect_input(input_string)
+    assert result == ['db_name', 'col_name']
